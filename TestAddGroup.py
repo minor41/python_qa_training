6888425aa90e2829f1dab3b644659c5d671ce27b
Piotr Stelmach <piotr.stelmach@xcaliber.com>
2020-03-29 22:48:29 +0200

    Version 1.4 fixing create group method

--- TestAddGroup.py
+++ TestAddGroup.py
@@ -15,8 +15,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd)
         self.open_groups_page(wd)
-        self.init_group_creation(wd)
-        self.fill_group_page(wd)
+        self.create_group(wd)
         self.submit_group_creation(wd)
         self.return_to_group_page(wd)
         self.logout(wd)
@@ -30,7 +29,10 @@ class TestAddGroup(unittest.TestCase):
     def submit_group_creation(self, wd):
         wd.find_element_by_name("submit").click()
 
-    def fill_group_page(self, wd):
+    def create_group(self, wd):
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group from
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("test 123")
@@ -40,9 +42,6 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("test 123")
-
-    def init_group_creation(self, wd):
-        wd.find_element_by_name("new").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
