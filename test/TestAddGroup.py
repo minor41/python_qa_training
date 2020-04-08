4802dc4a3856fe06915af9fdb0eeb09b0cc4ed55
Piotr Stelmach <piotr.stelmach@xcaliber.com>
2020-04-08 17:51:05 +0200

    Exercise num7 - adding edit contact and edit group tests fix

--- test/TestAddGroup.py
+++ test/TestAddGroup.py
@@ -4,12 +4,12 @@ from model.group import Group
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.contact(Group(name="test 123", header="test 123", footer="test 123"))
+    app.group.create(Group(name="test 123", header="test 123", footer="test 123"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.contact(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
