d2bb34631df8e3dd9a5b0d8d12c59c45fdca4b02
Piotr Stelmach <piotr.stelmach@xcaliber.com>
2020-04-28 00:50:34 +0200

    lesson 5 - 5.2 test phones

---
+++ test/TestPhones.py
@@ -0,0 +1,24 @@
+import re
+
+
+def test_phone_on_home_page(app):
+    contact_from_homepage = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_homepage.home_phone == clear(contact_from_edit_page.home_phone)
+    assert contact_from_homepage.mobile == clear(contact_from_edit_page.mobile)
+    assert contact_from_homepage.work == clear(contact_from_edit_page.work)
+    assert contact_from_homepage.phone2 == clear(contact_from_edit_page.phone2)
+
+
+def test_phone_on_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
+    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+    assert contact_from_view_page.work == contact_from_edit_page.work
+    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
