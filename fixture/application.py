f00458d0437bd3fe0eac0d490e12225f3ea1e903
Piotr Stelmach <piotr.stelmach@xcaliber.com>
2020-05-02 00:22:29 +0200

    Exercise 16

--- fixture/application.py
+++ fixture/application.py
@@ -6,11 +6,19 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +31,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url.endswith("/index.php") and
                 len(wd.find_elements_by_xpath("//img[@alt='Address book']")) > 0):
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
