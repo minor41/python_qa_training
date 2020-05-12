c0253f3fe8f60909acb2621286795567280f626c
Piotr Stelmach <piotr.stelmach@xcaliber.com>
2020-05-13 01:04:58 +0200

    adding changes to test for Groups add and delete, new fixture

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from model.contact import Contact
 from model.group import Group
 
 
@@ -9,7 +10,7 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password, autocommit=True)
 
     def get_group_list(self):
         list_group = []
@@ -22,6 +23,19 @@ class DbFixture:
         finally:
             cursor.close()
         return list_group
+
+    def get_contact_list(self):
+        list_contact = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, middlename, lastname from addressbook")
+            for row in cursor:
+                (id, firstname, middlename, lastname) = row
+                list_contact.append(Contact(contact_id=str(id), first_name=firstname, middle_name=middlename,
+                                            last_name=lastname))
+        finally:
+            cursor.close()
+        return list_contact
 
     def destroy(self):
         self.connection.close()
--- test/TestDbMatcherUi.py
+++ test/TestDbMatcherUi.py
@@ -1,3 +1,4 @@
+from model.contact import Contact
 from model.group import Group
 
 
@@ -8,3 +9,12 @@ def test_group_list(app, db):
         return Group(group_id=group.group_id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_con_list = app.contact.get_contact_list()
+
+    def clean(contact):
+        return Contact(contact_id=contact.contact_id)
+    db_con_list = map(clean, db.get_contact_list())
+    assert sorted(ui_con_list, key=Contact.id_or_max) == sorted(db_con_list, key=Contact.id_or_max)
